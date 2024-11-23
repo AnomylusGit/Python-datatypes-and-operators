1be9c55f788c422ea17147d1723fb6504614f743
AnomylusGit <urath.aarush13@gmail.com>
2024-11-23 13:57:43 +0200

    Datatypes, type casting and operators

---
+++ main.py
@@ -0,0 +1,25 @@
+name="Aarush"
+age=11
+weight=37.5
+studies=True
+print("My name is",name)
+print("Data type of my name is",type(name))
+
+print("My Age is",age)
+print("Data type of my age is",type(age))
+
+print("My weight is",weight)
+print("Data type of my weight is",type(weight))
+
+print("Do I study?",studies)
+print("Data type of do I study is",type(studies))
+
+print("\n After type casting")
+age=str(age)
+print("My Age is",age)
+print("Data type of my age is",type(age))
+
+print("\n After type casting")
+weight=int(weight)
+print("My weight is",weight)
+print("Data type of my weight is",type(weight))
